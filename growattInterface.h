c8220212e7b268cf89de0edb56104f41c29dbf29
Peter Zsak <wroadd@gmail.com>
2023-01-07 13:20:17 +0100

    added parameters for the 3 phases

--- growattInterface.h
+++ growattInterface.h
@@ -25,7 +25,7 @@ class growattIF {
     struct modbus_input_registers
     {
       int status;
-      float solarpower, pv1voltage, pv1current, pv1power, pv2voltage, pv2current, pv2power, outputpower, gridfrequency, gridvoltage;
+      float solarpower, pv1voltage, pv1current, pv1power, pv2voltage, pv2current, pv2power, outputpower, gridfrequency, gridvoltage, ph1voltage, ph1current, ph1power, ph2voltage, ph2current, ph2power, ph3voltage, ph3current, ph3power, lineph1voltage, lineph2voltage, lineph3voltage;
       float energytoday, energytotal, totalworktime, pv1energytoday, pv1energytotal, pv2energytoday, pv2energytotal, opfullpower;
       float tempinverter, tempipm, tempboost;
       int ipf, realoppercent, deratingmode, faultcode, faultbitcode, warningbitcode;
